ce6660ba2531d397352a0e7bc483bb7178365dde
Martin Rossil <martinrossil@gmail.com>
2021-01-05 12:15:54 +0100

    Created rollup and tsconfig files

---
+++ rollup.build.js
@@ -0,0 +1,17 @@
+import pkg from './package.json'
+import typescript from 'rollup-plugin-typescript2'
+import filesize from 'rollup-plugin-filesize';
+
+export default [
+    {
+        input: 'src/index.ts',
+        output: {
+            file: pkg.module,
+            format: 'esm',
+            sourcemap: true
+        },
+        plugins: [
+            typescript({ tsconfig: 'tsconfig.build.json' }),
+            filesize({ showBrotliSize: true })]
+    }
+]
---
+++ rollup.development.js
@@ -0,0 +1,23 @@
+import typescript from 'rollup-plugin-typescript2';
+import serve from 'rollup-plugin-serve';
+import livereload from 'rollup-plugin-livereload';
+import resolve from '@rollup/plugin-node-resolve';
+
+export default [{
+        input: './src/UnamiDev.ts',
+        plugins: [
+            typescript({ tsconfig: 'tsconfig.development.json' }),
+            resolve(),
+            serve({
+                contentBase: 'development',
+                open: true
+            }),
+            livereload()
+        ],
+        output: {
+            file: './development/esnext.js',
+            format: 'esm',
+            sourcemap: 'inline'
+        }
+    }
+]
