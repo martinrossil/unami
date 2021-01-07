9ba1324016768b6aad93578420e4ebc7f04b9409
Martin Rossil <martinrossil@gmail.com>
2021-01-07 23:54:18 +0100

    Created EventDispatcher.ts

---
+++ src/event/EventDispatcher.ts
@@ -0,0 +1,60 @@
+import IEventDispatcher from '../interfaces/event/IEventDispatcher';
+import IEventListener from '../interfaces/event/IEventListener';
+
+export default class EventDispatcher implements IEventDispatcher {
+    protected listeners: Map<string, IEventListener[] | undefined> = new Map();
+
+    public dispatchEvent(event: Event): boolean {
+        const typeListeners: IEventListener[] | undefined = this.listeners.get(event.type);
+        if (typeListeners !== undefined) {
+            for (const listener of typeListeners) {
+                listener(event);
+            }
+        }
+        return true;
+    }
+
+    public dispatchCustomEvent<Item>(type: string, payload: Item | undefined = undefined): void {
+        const typeListeners: IEventListener[] | undefined = this.listeners.get(type);
+        if (typeListeners !== undefined) {
+            const customEvent: CustomEvent<Item> = new CustomEvent<Item>(type, { detail: payload });
+            for (const listener of typeListeners) {
+                listener(customEvent);
+            }
+        }
+    }
+
+    public addEventListener(type: string, listener: IEventListener): void {
+        let typeListeners: IEventListener[] | undefined = this.listeners.get(type);
+        if (typeListeners === undefined) {
+            typeListeners = [];
+            this.listeners.set(type, typeListeners);
+        }
+        typeListeners.push(listener);
+    }
+
+    public removeEventListener(type: string, listener: IEventListener): void {
+        const typeListeners: IEventListener[] | undefined = this.listeners.get(type);
+        if (typeListeners !== undefined) {
+            for (const method of typeListeners) {
+                if (method === listener) {
+                    const index = typeListeners.indexOf(listener);
+                    typeListeners.splice(index, 1);
+                    break;
+                }
+            }
+        }
+    }
+
+    private _name = '';
+
+    public set name(value: string) {
+        if (this._name !== value) {
+            this._name = value;
+        }
+    }
+
+    public get name(): string {
+        return this._name;
+    }
+}
