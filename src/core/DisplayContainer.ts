8db64ceb34536ac646d2f12c445e3c7ba6924f1c
Martin Rossil <martinrossil@gmail.com>
2021-01-09 00:00:22 +0100

    Added addElement methods.

--- src/core/DisplayContainer.ts
+++ src/core/DisplayContainer.ts
@@ -13,7 +13,31 @@ export default class DisplayContainer extends DisplayElement implements IDisplay
     public addElement(element: IDisplayElement): void {
         this.elements.push(element);
         this.appendChild(element as unknown as Node);
-        // this.invalidateInternalSize();
+        this.invalidateInternalSize();
+    }
+
+    public addElements(elements: Array<IDisplayElement>): void {
+        const frag: DocumentFragment = document.createDocumentFragment();
+        for (const element of elements) {
+            this.elements.push(element);
+            frag.appendChild(element as unknown as Node);
+        }
+        this.appendChild(frag);
+        this.invalidateInternalSize();
+    }
+
+    protected updateInternalSize(): void {
+        let width = 0;
+        let height = 0;
+        for (const element of this.elements) {
+            if (width < element.measuredWidth) {
+                width = element.measuredWidth;
+            }
+            if (height < element.measuredHeight) {
+                height = element.measuredHeight;
+            }
+        }
+        this.internalSize(width, height);
     }
 }
 customElements.define('display-container', DisplayContainer);
