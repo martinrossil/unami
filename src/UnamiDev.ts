0782f9c90135361405b600be166b3fc182dc9523
Martin Rossil <martinrossil@gmail.com>
2021-01-09 00:01:31 +0100

    Added child elements for testing.

--- src/UnamiDev.ts
+++ src/UnamiDev.ts
@@ -1,14 +1,55 @@
+import ApplicationElement from './core/ApplicationElement';
 import DisplayContainer from './core/DisplayContainer';
+import DisplayElement from './core/DisplayElement';
+import IDisplayContainer from './interfaces/core/IDisplayContainer';
+import IDisplayElement from './interfaces/core/IDisplayElement';
 import IUnamiDev from './IUnamiDev';
 import Color from './vo/Color';
 
-export default class UnamiDev extends DisplayContainer implements IUnamiDev {
+export default class UnamiDev extends ApplicationElement implements IUnamiDev {
     public constructor() {
         super();
         this.name = 'UnamiDev';
         // hsla(210, 40%, 98%, 1)
-        this.size(200, 200);
-        this.backgroundColor = new Color(210, 100, 50);
+        this.backgroundColor = new Color(210, 100, 98);
+        this.addElement(this.dc);
+    }
+
+    private _dc!: IDisplayContainer;
+
+    private get dc(): IDisplayContainer {
+        if (!this._dc) {
+            this._dc = new DisplayContainer();
+            this._dc.name = 'red container';
+            this._dc.backgroundColor = new Color(0, 100, 0, 0.5);
+            this._dc.addElements([this.blue, this.orange]);
+            // this._dc.addElement(this.orange);
+        }
+        return this._dc;
+    }
+
+    private _blue!: IDisplayElement;
+
+    private get blue(): IDisplayElement {
+        if (!this._blue) {
+            this._blue = new DisplayElement();
+            this._blue.name = 'blue';
+            this._blue.size(300, 50);
+            this._blue.backgroundColor = new Color(210, 100, 50, 0.5);
+        }
+        return this._blue;
+    }
+
+    private _orange!: IDisplayElement;
+
+    private get orange(): IDisplayElement {
+        if (!this._orange) {
+            this._orange = new DisplayElement();
+            this._orange.name = 'orange';
+            this._orange.size(50, 300);
+            this._orange.backgroundColor = new Color(29, 100, 50, 0.5);
+        }
+        return this._orange;
     }
 }
 customElements.define('unami-dev', UnamiDev);
