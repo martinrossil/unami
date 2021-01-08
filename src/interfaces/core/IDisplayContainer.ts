1c0994c393c17d6a72368c5f4cf399e4f6db5fcd
Martin Rossil <martinrossil@gmail.com>
2021-01-09 00:01:10 +0100

    Added addElement methods.

--- src/interfaces/core/IDisplayContainer.ts
+++ src/interfaces/core/IDisplayContainer.ts
@@ -2,4 +2,5 @@ import IDisplayElement from './IDisplayElement';
 
 export default interface IDisplayContainer extends IDisplayElement {
     addElement(element: IDisplayElement): void;
+    addElements(elements: Array<IDisplayElement>): void;
 }
