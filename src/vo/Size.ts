94aaef7c519ed936c8c13501d9d9dfb5713613b8
Martin Rossil <martinrossil@gmail.com>
2021-01-13 14:35:48 +0100

    Created Size and interface.

---
+++ src/vo/Size.ts
@@ -0,0 +1,46 @@
+import ISize from '../interfaces/vo/ISize';
+
+export default class Size implements ISize {
+    public constructor(width: number, height: number) {
+        this.width = width;
+        this.height = height;
+    }
+
+    private _width = 0;
+
+    public set width(value: number) {
+        if (isNaN(value)) {
+            return;
+        }
+        if (value < 0) {
+            if (this._width !== 0) {
+                this._width = 0;
+                return;
+            }
+        }
+        this._width = value;
+    }
+
+    public get width(): number {
+        return this._width;
+    }
+
+    private _height = 0;
+
+    public set height(value: number) {
+        if (isNaN(value)) {
+            return;
+        }
+        if (value < 0) {
+            if (this._height !== 0) {
+                this._height = 0;
+                return;
+            }
+        }
+        this._height = value;
+    }
+
+    public get height(): number {
+        return this._height;
+    }
+}
