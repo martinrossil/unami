9719318708e9d63ca874513c56eb7319d68c609e
Martin Rossil <martinrossil@gmail.com>
2021-01-15 17:17:51 +0100

    Added color listeners.

--- src/core/DisplayElement.ts
+++ src/core/DisplayElement.ts
@@ -1,30 +1,50 @@
 import SizeElement from './SizeElement';
 import IDisplayElement from '../interfaces/core/IDisplayElement';
 import IColor from '../interfaces/vo/IColor';
+import ILinearGradient from '../interfaces/vo/ILinearGradient';
+import LinearGradient from '../vo/LinearGradient';
 
 export default class DisplayElement extends SizeElement implements IDisplayElement {
     public constructor() {
         super();
         this.name = 'DisplayElement';
+        this.backgroundColorChanged = this.backgroundColorChanged.bind(this);
         this.style.border = 'none';
         this.style.outline = 'none';
     }
 
-    private _backgroundColor: IColor | null = null;
+    private backgroundColorChanged(): void {
+        if (this.backgroundColor) {
+            if (this.backgroundColor instanceof LinearGradient) {
+                this.style.backgroundColor = '';
+                this.style.background = this.backgroundColor.toString();
+                return;
+            }
+            this.style.background = '';
+            this.style.backgroundColor = this.backgroundColor.toString();
+            return;
+        }
+        this.style.backgroundColor = '';
+        this.style.background = '';
+    }
 
-    public set backgroundColor(value: IColor | null) {
+    private _backgroundColor: IColor | ILinearGradient | null = null;
+
+    public set backgroundColor(value: IColor | ILinearGradient | null) {
         if (this._backgroundColor === value) {
             return;
         }
+        if (this._backgroundColor) {
+            this._backgroundColor.removeEventListener('invalidate', this.backgroundColorChanged);
+        }
         this._backgroundColor = value;
         if (this._backgroundColor) {
-            this.style.backgroundColor = this._backgroundColor.toString();
-        } else {
-            this.style.backgroundColor = '';
+            this._backgroundColor.addEventListener('invalidate', this.backgroundColorChanged);
         }
+        this.backgroundColorChanged();
     }
 
-    public get backgroundColor(): IColor | null {
+    public get backgroundColor(): IColor | ILinearGradient | null {
         return this._backgroundColor;
     }
 }
--- src/interfaces/core/IDisplayElement.ts
+++ src/interfaces/core/IDisplayElement.ts
@@ -1,6 +1,7 @@
 import IColor from '../vo/IColor';
+import ILinearGradient from '../vo/ILinearGradient';
 import ISizeElement from './ISizeElement';
 
 export default interface IDisplayElement extends ISizeElement {
-    backgroundColor: IColor | null;
+    backgroundColor: IColor | ILinearGradient | null;
 }
