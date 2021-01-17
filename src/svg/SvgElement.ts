67a31b4a099fa072a1d4ca32bf14d674201d235a
Martin Rossil <martinrossil@gmail.com>
2021-01-17 14:16:12 +0100

    Created SvgElement.

---
+++ src/svg/SvgElement.ts
@@ -0,0 +1,72 @@
+import SizeElement from '../core/SizeElement';
+import ISvgElement from '../interfaces/svg/ISvgElement';
+import IRectangle from '../interfaces/vo/IRectangle';
+
+export default class SvgElement extends SizeElement implements ISvgElement {
+    public constructor() {
+        super();
+        this.name = 'SvgElement';
+        this.appendChild(this.svg);
+    }
+
+    protected validate(): void {
+        super.validate();
+        this.updateSvgAttributes();
+    }
+
+    protected updateSvgAttributes(): void {
+        this.svg.setAttribute('width', this.measuredWidth.toString());
+        this.svg.setAttribute('height', this.measuredHeight.toString());
+    }
+
+    private _svg!: SVGSVGElement;
+
+    private get svg(): SVGSVGElement {
+        if (!this._svg) {
+            this._svg = document.createElementNS('http://www.w3.org/2000/svg', 'svg');
+            this._svg.style.position = 'absolute';
+            this._svg.style.overflow = 'visible';
+            this._svg.appendChild(this.defs);
+            this._svg.appendChild(this.group);
+        }
+        return this._svg;
+    }
+
+    private _defs!: SVGDefsElement;
+
+    protected get defs(): SVGDefsElement {
+        if (!this._defs) {
+            this._defs = document.createElementNS('http://www.w3.org/2000/svg', 'defs');
+        }
+        return this._defs;
+    }
+
+    private _group!: SVGElement;
+
+    protected get group(): SVGElement {
+        if (!this._group) {
+            this._group = document.createElementNS('http://www.w3.org/2000/svg', 'g');
+        }
+        return this._group;
+    }
+
+    private _viewBox: IRectangle | null = null;
+
+    public set viewBox(value: IRectangle | null) {
+        if (this._viewBox === value) {
+            return;
+        }
+        this._viewBox = value;
+        if (this._viewBox) {
+            const box = this._viewBox;
+            this.svg.setAttribute('viewBox', box.x + ' ' + box.y + ' ' + box.width + ' ' + box.height);
+            return;
+        }
+        this.svg.removeAttribute('viewBox');
+    }
+
+    public get viewBox(): IRectangle | null {
+        return this._viewBox;
+    }
+}
+customElements.define('svg-element', SvgElement);
