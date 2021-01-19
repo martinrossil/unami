f594a0c762eeca7e33b418220d1b15c917785a5d
Martin Rossil <martinrossil@gmail.com>
2021-01-19 17:10:38 +0100

    Refactored filters.

--- src/interfaces/svg/ISvgElement.ts
+++ src/interfaces/svg/ISvgElement.ts
@@ -1,6 +1,10 @@
+import BlurFilter from '../../filters/BlurFilter';
+import ShadowFilter from '../../filters/ShadowFilter';
 import ISizeElement from '../core/ISizeElement';
 import IRectangle from '../vo/IRectangle';
 
 export default interface ISvgElement extends ISizeElement {
     viewBox: IRectangle | null;
+    visible: boolean;
+    addFilter(filter: BlurFilter | ShadowFilter): void;
 }
--- src/svg/PathElement.ts
+++ src/svg/PathElement.ts
@@ -1,7 +1,7 @@
-import IFilter from '../interfaces/filters/IFilter';
 import IPathElement from '../interfaces/svg/IPathElement';
 import IColor from '../interfaces/vo/IColor';
 import ILinearGradient from '../interfaces/vo/ILinearGradient';
+import LinearGradient from '../vo/LinearGradient';
 import SvgElement from './SvgElement';
 
 export default class PathElement extends SvgElement implements IPathElement {
@@ -10,16 +10,7 @@ export default class PathElement extends SvgElement implements IPathElement {
         this.name = 'PathElement';
         this.strokeColorChanged = this.strokeColorChanged.bind(this);
         this.fillColorChanged = this.fillColorChanged.bind(this);
-        this.filtersChanged = this.filtersChanged.bind(this);
         this.group.appendChild(this.path);
-    }
-
-    private filters: Array<IFilter> = [];
-
-    public addFilter(value: IFilter): void {
-        this.filters.push(value);
-        value.addEventListener('invalidate', this.filtersChanged);
-        this.filtersChanged();
     }
 
     private strokeColorChanged(): void {
@@ -31,26 +22,39 @@ export default class PathElement extends SvgElement implements IPathElement {
     }
 
     private fillColorChanged(): void {
+        console.log(this.name, 'fillColorChanged()');
         if (this.fillColor) {
+            if (this.fillColor instanceof LinearGradient) {
+                // this.defs.contains
+                this.fillLinearGradient.setAttribute('gradientTransform', 'rotate(' + this.fillColor.degrees + ')'); // gradientTransform="rotate(90)"
+                return;
+            }
             this.path.setAttribute('fill', this.fillColor.toString());
             return;
         }
         this.path.removeAttribute('fill');
     }
 
-    private filtersChanged(): void {
-        console.log(this.name, 'filtersChanged()');
-        let filterString = '';
-        if (this.filters.length === 0) {
-            this.style.filter = filterString;
-            return;
+    private fillLinearGradientId = Math.random().toString();
+
+    private _fillLinearGradient!: SVGLinearGradientElement
+
+    private get fillLinearGradient(): SVGLinearGradientElement {
+        if (!this._fillLinearGradient) {
+            this._fillLinearGradient = document.createElementNS('http://www.w3.org/2000/svg', 'linearGradient');
+            this._fillLinearGradient.id = this.fillLinearGradientId;
+            this._fillLinearGradient.setAttribute('gradientUnits', 'userSpaceOnUse');
+            // this._fillLinearGradient.setAttribute('id', this.fillLinearGradientId);
+            const stop: SVGStopElement = document.createElementNS('http://www.w3.org/2000/svg', 'stop');
+            stop.setAttribute('stop-color', 'red');
+            stop.setAttribute('offset', '0.0');
+            const stop2: SVGStopElement = document.createElementNS('http://www.w3.org/2000/svg', 'stop');
+            stop2.setAttribute('stop-color', 'yellow');
+            stop2.setAttribute('offset', '1.0');
+            this._fillLinearGradient.appendChild(stop);
+            this._fillLinearGradient.appendChild(stop2);
         }
-        for (const filter of this.filters) {
-            console.log('filter', filter.toString());
-            filterString += filter.toString() + ' ';
-        }
-        this.style.filter = filterString.substr(0, filterString.length - 2);
-        console.log('filter', this.style.filter);
+        return this._fillLinearGradient;
     }
 
     private _pathData = '';
@@ -69,7 +73,7 @@ export default class PathElement extends SvgElement implements IPathElement {
 
     private _path!: SVGPathElement;
 
-    private get path(): SVGPathElement {
+    protected get path(): SVGPathElement {
         if (!this._path) {
             this._path = document.createElementNS('http://www.w3.org/2000/svg', 'path');
         }
@@ -103,10 +107,18 @@ export default class PathElement extends SvgElement implements IPathElement {
             return;
         }
         if (this._fillColor) {
+            /* if (this._fillColor instanceof LinearGradient) {
+                this.fillLinearGradient.
+                this.defs.removeChild(this.fillLinearGradient);
+            } */
             this._fillColor.removeEventListener('invalidate', this.fillColorChanged);
         }
         this._fillColor = value;
         if (this._fillColor) {
+            if (this._fillColor instanceof LinearGradient) {
+                this.defs.appendChild(this.fillLinearGradient);
+                this.path.setAttribute('fill', "url('#" + this.fillLinearGradientId + "')");
+            }
             this._fillColor.addEventListener('invalidate', this.fillColorChanged);
         }
         this.fillColorChanged();
@@ -114,6 +126,27 @@ export default class PathElement extends SvgElement implements IPathElement {
 
     public get fillColor(): IColor | ILinearGradient | null {
         return this._fillColor;
+    }
+
+    private _strokeWidth = 0;
+
+    public set strokeWidth(value: number) {
+        if (this._strokeWidth === value) {
+            return;
+        }
+        if (isNaN(value) || value < 0) {
+            if (this._strokeWidth !== 0) {
+                this._strokeWidth = 0;
+                this.path.removeAttribute('stroke-width');
+            }
+            return;
+        }
+        this._strokeWidth = value;
+        this.path.setAttribute('stroke-width', this._strokeWidth.toString());
+    }
+
+    public get strokeWidth(): number {
+        return this._strokeWidth;
     }
 }
 customElements.define('path-element', PathElement);
