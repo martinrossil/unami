a427a2f7d39ba701ba18b8d968f40b49f79b2a81
Martin Rossil <martinrossil@gmail.com>
2021-01-19 17:09:45 +0100

    Implemented stroke- and fillColor.

--- src/interfaces/svg/ISurfaceElement.ts
+++ src/interfaces/svg/ISurfaceElement.ts
@@ -1,7 +1,15 @@
+import BlurFilter from '../../filters/BlurFilter';
+import ShadowFilter from '../../filters/ShadowFilter';
 import { CornerType } from '../../types/CornerType';
-import ISvgElement from './ISvgElement';
+import ISizeElement from '../core/ISizeElement';
+import IColor from '../vo/IColor';
+import ILinearGradient from '../vo/ILinearGradient';
 
-export default interface ISurfaceElement extends ISvgElement {
+export default interface ISurfaceElement extends ISizeElement {
     cornerSize: number;
     cornerType: CornerType;
+    strokeColor: IColor | ILinearGradient | null;
+    fillColor: IColor | ILinearGradient | null;
+    strokeWidth: number;
+    addFilter(filter: BlurFilter | ShadowFilter): void;
 }
