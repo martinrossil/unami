24a6923b474732d451a99e63f73cc968783b0b51
Martin Rossil <martinrossil@gmail.com>
2021-01-20 23:11:30 +0100

    Added readonly colors.

--- src/interfaces/vo/ILinearGradient.ts
+++ src/interfaces/vo/ILinearGradient.ts
@@ -3,6 +3,7 @@ import IColor from './IColor';
 
 export default interface ILinearGradient extends IEventDispatcher {
     degrees: number;
+    readonly colors: Array<IColor>;
     addColor(color: IColor): void;
     addColors(colors: Array<IColor>): void;
     toString(): string;
