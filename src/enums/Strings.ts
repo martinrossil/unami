2cfbcfbeb28abb347ee4cd641088f98b23845202
Martin Rossil <martinrossil@gmail.com>
2021-01-21 13:15:30 +0100

    Created Strings enum.

---
+++ src/enums/Strings.ts
@@ -0,0 +1,19 @@
+export const enum Strings {
+    D = 'd',
+    FILL = 'fill',
+    GRADIENT_TRANSFORM = 'gradientTransform',
+    GRADIENT_UNITS = 'gradientUnits',
+    ID = 'id',
+    LINEAR_GRADIENT = 'linearGradient',
+    OFFSET = 'offset',
+    PATH = 'path',
+    ROTATE = 'rotate',
+    STOP = 'stop',
+    STOP_COLOR = 'stop-color',
+    STROKE = 'stroke',
+    STROKE_COLOR = 'stroke-color',
+    STROKE_WIDTH = 'stroke-width',
+    SVG_NS = 'http://www.w3.org/2000/svg',
+    URL = 'url',
+    USER_SPACE_ON_USE = 'userSpaceOnUse'
+}
