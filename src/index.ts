02a2398ddd9e4d5d543c052a6e4f3035a6c1d1cc
Martin Rossil <martinrossil@gmail.com>
2021-01-26 11:56:50 +0100

    Exported all classes and interfaces in index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,80 @@
-export { default as UnamiDev } from './UnamiDev';
+export { default as ApplicationElement } from './core/ApplicationElement';
+export { default as BaseElement } from './core/BaseElement';
+export { default as DisplayContainer } from './core/DisplayContainer';
+export { default as DisplayElement } from './core/DisplayElement';
+export { default as PositionElement } from './core/PositionElement';
+export { default as ScrollContainer } from './core/ScrollContainer';
+export { default as SizeElement } from './core/SizeElement';
+export { default as ArrayCollection } from './data/ArrayCollection';
+
+export { Strings } from './enums/Strings';
+
+export { default as EventDispatcher } from './event/EventDispatcher';
+
+export { default as BlurFilter } from './filters/BlurFilter';
+export { default as BoxShadowFilter } from './filters/BoxShadowFilter';
+export { default as ShadowFilter } from './filters/ShadowFilter';
+
+export { default as Machine } from './fsm/Machine';
+export { default as State } from './fsm/State';
+
+export { default as IBaseElement } from './interfaces/core/IBaseElement';
+export { default as IDisplayContainer } from './interfaces/core/IDisplayContainer';
+export { default as IDisplayElement } from './interfaces/core/IDisplayElement';
+export { default as IPositionElement } from './interfaces/core/IPositionElement';
+export { default as IScrollContainer } from './interfaces/core/IScrollContainer';
+export { default as ISizeElement } from './interfaces/core/ISizeElement';
+
+export { default as IArrayCollection } from './interfaces/data/IArrayCollection';
+
+export { default as IEventDispatcher } from './interfaces/event/IEventDispatcher';
+export { default as IEventListener } from './interfaces/event/IEventListener';
+
+export { default as IBlurFilter } from './interfaces/filters/IBlurFilter';
+export { default as IBoxShadowFilter } from './interfaces/filters/IBoxShadowFilter';
+export { default as IShadowFilter } from './interfaces/filters/IShadowFilter';
+
+export { default as IState } from './interfaces/fsm/IState';
+
+export { default as IHorizontalLayout } from './interfaces/layout/IHorizontalLayout';
+export { default as ILayout } from './interfaces/layout/ILayout';
+export { default as IVerticalLayout } from './interfaces/layout/IVerticalLayout';
+
+export { default as IPathElement } from './interfaces/svg/IPathElement';
+export { default as ISurfaceElement } from './interfaces/svg/ISurfaceElement';
+export { default as ISvgElement } from './interfaces/svg/ISvgElement';
+
+export { default as IBaseText } from './interfaces/text/IBaseText';
+export { default as ILabelElement } from './interfaces/text/ILabelElement';
+export { default as ITextElement } from './interfaces/text/ITextElement';
+export { default as ITextRenderer } from './interfaces/text/ITextRenderer';
+
+export { default as IColor } from './interfaces/vo/IColor';
+export { default as ILinearGradient } from './interfaces/vo/ILinearGradient';
+export { default as IRectangle } from './interfaces/vo/IRectangle';
+export { default as ISize } from './interfaces/vo/ISize';
+export { default as ITypeFace } from './interfaces/vo/ITypeFace';
+
+export { default as AbsoluteLayout } from './layout/AbsoluteLayout';
+export { default as HorizontalLayout } from './layout/HorizontalLayout';
+export { default as VerticalLayout } from './layout/VerticalLayout';
+
+export { default as PathElement } from './svg/PathElement';
+export { default as SurfaceElement } from './svg/SurfaceElement';
+export { default as SvgElement } from './svg/SvgElement';
+
+export { default as BaseText } from './text/BaseText';
+export { default as LabelElement } from './text/LabelElement';
+export { default as TextElement } from './text/TextElement';
+export { default as TextRenderer } from './text/TextRenderer';
+
+export { CornerType } from './types/CornerType';
+export { HorizontalAlign } from './types/HorizontalAlign';
+export { TextAlign } from './types/TextAlign';
+export { VerticalAlign } from './types/VerticalAlign';
+
+export { default as Color } from './vo/Color';
+export { default as LinearGradient } from './vo/LinearGradient';
+export { default as Rectangle } from './vo/Rectangle';
+export { default as Size } from './vo/Size';
+export { default as TypeFace } from './vo/TypeFace';
