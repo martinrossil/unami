5222a0f14993c9b65ecdea40b076537143bb7efb
Martin Rossil <martinrossil@gmail.com>
2021-02-12 13:22:46 +0100

    Testing BottomNavigation.

--- src/UnamiDev.ts
+++ src/UnamiDev.ts
@@ -11,28 +11,36 @@ import IArrayCollection from './interfaces/data/IArrayCollection';
 import IEventListener from './interfaces/event/IEventListener';
 import ILabelElement from './interfaces/text/ILabelElement';
 import IUnamiDev from './IUnamiDev';
+import HorizontalLayout from './layout/HorizontalLayout';
 import VerticalLayout from './layout/VerticalLayout';
 import LabelElement from './text/LabelElement';
 import Color from './vo/Color';
 import TypeFace from './vo/TypeFace';
 import { HeroIcon } from './zapp/enums/HeroIcon';
 import INavigationItem from './zapp/interfaces/vo/INavigationItem';
+import BottomNavigationItemRenderer from './zapp/navigation/BottomNavigationItemRenderer';
 import NavigationItemRenderer from './zapp/navigation/NavigationItemRenderer';
-import NaviagtionItem from './zapp/vo/NavigationItem';
+import NavigationItem from './zapp/vo/NavigationItem';
 
 export default class UnamiDev extends ApplicationElement implements IUnamiDev {
     public constructor() {
         super();
         this.name = 'UnamiDev';
-        this.backgroundColor = this.colors.neutral.c100;
-        this.theme.typography.primary = new TypeFace('Bitter', 0.71, 0.03, 0.02);
+        this.backgroundColor = new Color(218, 60, 8);
+        // this.backgroundColor = this.colors.neutral.c100;
+        // this.theme.typography.primary = new TypeFace('Bitter', 0.71, 0.03, 0.02);
+        this.theme.typography.primary = new TypeFace('Eurostile Extended', 0.68, 0.09, 0.025);
         this.theme.typography.secondary = new TypeFace('Inter', 0.727, 0.09, 0.0);
-        // this.layout = new VerticalLayout(24, 'center', 'middle');
+        // this.theme.typography.secondary = new TypeFace('Eurostile', 0.67, 0.06, 0.01);
+        this.layout = new VerticalLayout(200, 'center', 'middle');
         window.addEventListener('load', () => {
-            this.addElement(this.list);
+            // this.addElement(this.list);
+            this.addElement(this.bottomNavigationList);
+            // this.addElement(this.labelElement);
+            // this.addElement(this.labelElement2);
         });
         window.addEventListener('click', () => {
-            this.list.dataProvider = this.navigationItems;
+            // this.list.dataProvider = this.navigationItems;
         });
 
         this.addEventListener('triggered', (e: Event) => {
@@ -48,6 +56,54 @@ export default class UnamiDev extends ApplicationElement implements IUnamiDev {
         console.log('unami', this.name, e.type, e.detail);
     }
 
+    private _labelElement!: ILabelElement;
+
+    private get labelElement(): ILabelElement {
+        if (!this._labelElement) {
+            this._labelElement = new LabelElement();
+            this._labelElement.text = 'TRON 80';
+            this._labelElement.fontSize = 50;
+            this._labelElement.fontWeight = 400;
+            this._labelElement.letterSpacing = 10;
+            this._labelElement.textColor = new Color(18, 100, 46); // hsl(18,100,46)
+            this._labelElement.typeFace = this.theme.typography.primary;
+        }
+        return this._labelElement;
+    }
+
+    private _labelElement2!: ILabelElement;
+
+    private get labelElement2(): ILabelElement {
+        if (!this._labelElement2) {
+            this._labelElement2 = new LabelElement();
+            this._labelElement2.text = 'TRON 80';
+            this._labelElement2.fontSize = 50;
+            this._labelElement2.fontWeight = 700;
+            this._labelElement2.letterSpacing = 10;
+            this._labelElement2.textColor = new Color(0, 100, 38); // hsl(0,100,38) red new Color(50, 98, 51); // yellow new Color(204, 97, 48);light blue
+            this._labelElement2.typeFace = this.theme.typography.secondary;
+        }
+        return this._labelElement2;
+    }
+
+    private _bottomNavigationList!: IList<NavigationItem>;
+
+    private get bottomNavigationList(): IList<NavigationItem> {
+        if (!this._bottomNavigationList) {
+            this._bottomNavigationList = new List();
+            this._bottomNavigationList.selectedIndex = 0;
+            this._bottomNavigationList.height = 56;
+            this._bottomNavigationList.percentWidth = 100;
+            this._bottomNavigationList.backgroundColor = this.colors.primary.c700;
+            this._bottomNavigationList.addFilter(new BoxShadowFilter(0, -4, 6, -1, new Color(0, 0, 0, 0.1)));
+            this._bottomNavigationList.addFilter(new BoxShadowFilter(0, -2, 4, -1, new Color(0, 0, 0, 0.06)));
+            this._bottomNavigationList.dataProvider = this.navigationItems;
+            this._bottomNavigationList.ItemRendererClass = BottomNavigationItemRenderer;
+            this._bottomNavigationList.layout = new HorizontalLayout(0, 'center', 'middle');
+        }
+        return this._bottomNavigationList;
+    }
+
     private _list!: IList<INavigationItem>;
 
     private get list(): IList<INavigationItem> {
@@ -55,15 +111,15 @@ export default class UnamiDev extends ApplicationElement implements IUnamiDev {
             this._list = new List();
             this._list.backgroundColor = this.colors.primary.c700;
             // this._list.cornerSize = 4;
-            this._list.selectedIndex = 5;
+            this._list.selectedIndex = 0;
             this._list.padding = 8;
             this._list.width = 200;
             this._list.percentHeight = 100;
             this._list.ItemRendererClass = NavigationItemRenderer;
-            // this._list.dataProvider = this.navigationItems;
+            this._list.dataProvider = this.navigationItems;
             this._list.layout = new VerticalLayout(8, 'fill');
-            this._list.addFilter(new BoxShadowFilter(0, 4, 6, -1, new Color(0, 0, 0, 0.1)));
-            this._list.addFilter(new BoxShadowFilter(0, 2, 4, -1, new Color(0, 0, 0, 0.06)));
+            // this._list.addFilter(new BoxShadowFilter(0, 4, 6, -1, new Color(0, 0, 0, 0.1)));
+            // this._list.addFilter(new BoxShadowFilter(0, 2, 4, -1, new Color(0, 0, 0, 0.06)));
             this._list.horizontalScrollEnabled = false;
             this._list.verticalScrollEnabled = true;
             this._list.addEventListener('selectedItemChanged', this.selectedItemChanged.bind(this) as IEventListener);
@@ -77,26 +133,11 @@ export default class UnamiDev extends ApplicationElement implements IUnamiDev {
     private get navigationItems(): IArrayCollection<INavigationItem> {
         if (!this._navigationItems) {
             this._navigationItems = new ArrayCollection([
-                new NaviagtionItem('Home', HeroIcon.HOME),
-                new NaviagtionItem('Blog', HeroIcon.PENCIL_ALT),
-                new NaviagtionItem('About', HeroIcon.USER),
-                new NaviagtionItem('Work', HeroIcon.TERMINAL),
-                new NaviagtionItem('Design', HeroIcon.SPARKLES),
-                new NaviagtionItem('Home', HeroIcon.HOME),
-                new NaviagtionItem('Blog', HeroIcon.PENCIL_ALT),
-                new NaviagtionItem('About', HeroIcon.USER),
-                new NaviagtionItem('Work', HeroIcon.TERMINAL),
-                new NaviagtionItem('Design', HeroIcon.SPARKLES),
-                new NaviagtionItem('Home', HeroIcon.HOME),
-                new NaviagtionItem('Blog', HeroIcon.PENCIL_ALT),
-                new NaviagtionItem('About', HeroIcon.USER),
-                new NaviagtionItem('Work', HeroIcon.TERMINAL),
-                new NaviagtionItem('Design', HeroIcon.SPARKLES),
-                new NaviagtionItem('Home', HeroIcon.HOME),
-                new NaviagtionItem('Blog', HeroIcon.PENCIL_ALT),
-                new NaviagtionItem('About', HeroIcon.USER),
-                new NaviagtionItem('Work', HeroIcon.TERMINAL),
-                new NaviagtionItem('Design', HeroIcon.SPARKLES)
+                new NavigationItem('Home', HeroIcon.HOME),
+                new NavigationItem('Blog', HeroIcon.PENCIL_ALT),
+                new NavigationItem('About', HeroIcon.USER),
+                new NavigationItem('Work', HeroIcon.TERMINAL),
+                new NavigationItem('Design', HeroIcon.SPARKLES)
             ]);
         }
         return this._navigationItems;
@@ -109,18 +150,6 @@ export default class UnamiDev extends ApplicationElement implements IUnamiDev {
             this._button = new Button();
         }
         return this._button;
-    }
-
-    private _labelElement!: ILabelElement;
-
-    private get labelElement(): ILabelElement {
-        if (!this._labelElement) {
-            this._labelElement = new LabelElement();
-            this._labelElement.text = 'Lorem Ipsum';
-            this._labelElement.fontSize = 56;
-            this._labelElement.typeFace = this.theme.typography.primary;
-        }
-        return this._labelElement;
     }
 
     private _badge!: IBadge;
